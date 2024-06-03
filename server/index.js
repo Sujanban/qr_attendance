a4c5204f9ff71fc70fec9a1b90e2a5ac37171d7b
Sujan Ban <bansujan@gmail.com>
2024-06-03 20:37:50 +0545

    test route added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 
-
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
